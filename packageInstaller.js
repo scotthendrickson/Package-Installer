fc1b55e68dab4c93753801c0bcb5e3f2afdaa210
Scott Hendrickson <shendrickson@localhost.localdomain>
2017-06-22 09:04:33 -0600

    Starting function by taking an array and joining it

--- packageInstaller.js
+++ packageInstaller.js
@@ -1,3 +1,12 @@
-/**
- * Created by shendrickson on 6/21/17.
- */
+//Need to write a function that when given an array returns a string.
+var testArray = ["KittenService: ", "Leetmeme: CyberPortal", "CyberPortal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: "];
+
+//To start I'm just going to create a function that takes the array, joins it and then returns a string
+function packageInstaller(inputArray) {
+    return inputArray.join("").replace(": ", ", ");
+}
+
+//Expected outcome is KittenService, Leetmeme, CyperPortalCypberPortal, IceCamelCaser, KittenServiceFraudstream, LeetmemeIce,
+console.log(packageInstaller(testArray));
+
+//Returned KittenService, Leetmeme: CyberPortalCyberPortal: IceCamelCaser: KittenServiceFraudstream: LeetmemeIce: so I wasn't quiet correct in what I thought I'd get.
